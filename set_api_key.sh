edfd2073019c0cef6caed7612c856ac40c0e16f6
Mike Sheldon <mike@mikeasoft.com>
2020-06-22 14:34:53 +0100

    Add OSM repository files

---
+++ set_api_key.sh
@@ -0,0 +1,10 @@
+if [ -z "$1" ] 
+then
+    echo "Usage: ./set_api_key.sh your_api_key"
+    exit -1
+fi
+echo "Setting API key to $1"
+for file in cycle cycle-hires hiking hiking-hires night-transit night-transit-hires satellite street street-hires terrain terrain-hires transit transit-hires
+do
+    sed -i s/$1/g $file
+done
