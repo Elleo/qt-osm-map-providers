a8471a5acbe61a6e4f4ac042abe7862aad4e0c22
Mike Sheldon <mike@mikeasoft.com>
2020-07-17 18:53:14 +0100

    Make sed usage compatible with Mac OSX

--- set_api_key.sh
+++ set_api_key.sh
@@ -1,4 +1,4 @@
-if [ -z "$1" ] 
+if [ -z "$1" ]
 then
     echo "Usage: ./set_api_key.sh your_api_key"
     exit -1
@@ -6,5 +6,5 @@ fi
 echo "Setting API key to $1"
 for file in cycle cycle-hires hiking hiking-hires night-transit night-transit-hires satellite street street-hires terrain terrain-hires transit transit-hires
 do
-    sed -i "s/YOUR_API_KEY/$1/g" $file
+    sed -i -- "s/YOUR_API_KEY/$1/g" $file
 done
